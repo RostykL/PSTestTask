d186265d99f3e636e0fdc25b698d8312b0ff9138
RostykL <rostyk.lukavyi@redvike.com>
2023-11-20 21:15:55 +0200

    change output directory name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   mode: "development",
   entry: "/src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     publicPath: "/",
     filename: "bundle.js",
   },
